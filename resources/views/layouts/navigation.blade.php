6695b430975679daa76d55bab2e50cbcc9fbed5e
Juan Angela Alma <juanlah77@gmail.com>
2023-07-24 09:13:14 +0700

    Changed Logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,9 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('books.table') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <div class="w-[100px]">
+                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        </div>
                     </a>
                 </div>
 
